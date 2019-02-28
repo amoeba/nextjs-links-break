408d48d92354f55a2173fcdf05c337c254d8f8d8
Bryce Mecum <petridish@gmail.com>
2019-02-27 21:14:17 -0900

    Iniital commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default () => (<div>
+  <ul>
+    <li>
+      <Link href='/' as='/'>
+        <a>/</a>
+      </Link>
+    </li>
+    <li>
+      <Link href='/a' as='/a'>
+        <a>a</a>
+      </Link>
+    </li>
+    <li>
+      <Link href='/b' as='/b'>
+        <a>b</a>
+      </Link>
+    </li>
+  </ul>
+</div>
+)
